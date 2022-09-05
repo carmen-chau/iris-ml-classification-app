b2ea2e8710ba611ae3a6a4d4ef136f65fcfe7bb7
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2022-09-04 20:57:21 -0400

    Updated new files

--- static/contactPage.js
+++ static/contactPage.js
@@ -3,13 +3,16 @@ const submittedForm = document.getElementById("contactForm");
 
 submittedForm.addEventListener("submit", function(event){
     const userEmail = document.getElementById("email").value
-    const userMessage = document.getElementById("message").value
+    const userFirstName = document.getElementById("firstName").value
+    const userLastName = document.getElementById("lastName").value
     if (!(userEmail.includes("@"))){
         alert("You have entered an invalid email!")
         event.preventDefault(); // prevents the form from submitting if there is an invalid email
     }
+    //storing user information for the personalized "Thank you for submitting" page
+    sessionStorage.setItem("userFirstName", userFirstName)
+    sessionStorage.setItem("userLastName", userLastName)
 })
-
 
 /*
 let userDataStorage = [];
---
+++ static/thankYou.js
@@ -0,0 +1,17 @@
+//firstName = JSON.stringify(sessionStorage.getItem("userFirstName"))
+//lastName = JSON.stringify(sessionStorage.getItem("userLastName"))
+
+firstName = sessionStorage.getItem("userFirstName")
+lastName = sessionStorage.getItem("userLastName")
+
+//sessionStorage.getItem("userLastName")
+
+document.getElementById("firstName").innerText = (firstName)
+document.getElementById("lastName").innerText = (lastName)
+
+/*
+
+document.getElementById("firstName").innerText = (firstName).slice(3,firstName.length)
+document.getElementById("firstName").innerText = (lastName).slice(3,lastName.length)
+
+*/
