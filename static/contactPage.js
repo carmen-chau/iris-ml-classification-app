0220070c2c507e44d79697dee3f79d1973918e4e
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2022-09-18 23:43:16 -0400

    Updated contactPage.js and all html files

--- static/contactPage.js
+++ static/contactPage.js
@@ -1,4 +1,3 @@
-//let userDataStorage = [];
 const submittedForm = document.getElementById("contactForm");
 
 submittedForm.addEventListener("submit", function(event){
@@ -13,33 +12,3 @@ submittedForm.addEventListener("submit", function(event){
     sessionStorage.setItem("userFirstName", userFirstName)
     sessionStorage.setItem("userLastName", userLastName)
 })
-
-/*
-let userDataStorage = [];
-document.getElementById("contactForm").addEventListener("submit", function(event){
-    event.preventDefault(); //prevents the form from auto-reoloading after submission. auto-reloading would inhibit ability to store user input
-    let userData = { //create an object to store user input information
-        firstName: document.getElementById("firstName").value,
-        lastName: document.getElementById("lastName").value,
-        emailAddress: document.getElementById("email").value,
-        message: document.getElementById("message").value
-    }
-
-    userDataStorage.push(userData); //add element to array
-    document.querySelector("form").reset(); //Reset the form for the next entry
-    //storing user input in localStorage
-    localStorage.setItem("User information", JSON.stringify(userDataStorage));
-});
-
-
-/*
-const form = document.getElementById('contactForm');
-const log = document.getElementById('log');
-form.addEventListener('submit', logSubmit);
-
-function logSubmit(event) {
-    //log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
-    alert("Hello! I am an alert box!");
-    event.preventDefault();
-  }
-*/
